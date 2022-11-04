1a537674830ac64cfcfed9ab3e3e425847fd9e7a
Rafael De Assis Ramalho Lima <de-assis-ramalho-lim@royal-14.cs.wisc.edu>
2022-11-03 21:36:46 -0500

    Skeleton code

--- psort.c
+++ psort.c
@@ -2,29 +2,37 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
 #include <sys/sysinfo.h>
 #include <sys/stat.h>
 #include <sys/mman.h>
-#include <unistd.h>
 
-/**
- * Error action
- * 
- */
-void error(){
-    char error_message[30] = "An error has occurred\n";
-    write(STDERR_FILENO, error_message, strlen(error_message));
+typedef struct rec {
+    int key;
+    int value[24]; // 96 bytes
+} rec_t;
+
+void* sort(rec_t records[]) {
+    // merge sort
+
+    return ;
 }
 
 int main(int argc, char *argv[]){
 // wrong invokation
     if(argc!=3){
-        error();
         exit(1);
     }
 
     const char *input = argv[1];
     const char *output = argv[2];
-
+    int nprocs = get_nprocs();
+    pthread_t cid[nprocs];
+    rec_t *records[];
+    // get file, divide into chunks;
+    for (int i = 0; i < nprocs; i++)
+        pthread_create(&cid[i], NULL, sort, &records[i]);
+    
     return 0;
 }
